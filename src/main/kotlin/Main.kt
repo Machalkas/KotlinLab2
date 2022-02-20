7842bd84710385248024e06d5c18b54519ef3b5f
Machalka <al1999dk@gmail.com>
2022-02-20 23:20:22 +0300

    правки

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,31 +3,26 @@ import java.io.File
 val file_name="students"
 val path="./src/main/files/"
 fun main() {
-//    val g= arrayOf(Grade("math",5))
-//    val st=Student("kek","11",g)
-//    val stud=Students(arrayOf(st))
-//    print(stud)
     val stud=decodeJson(readFile("$path$file_name.json"))
     print(stud)
-    stud.students[0].name="Василий"
-    for (i in stud.students){
-        for (j in i.grades) {
-            if(j.subject=="history") j.mark=4
+    stud.students[0].name="Василий" //Изменяем имя первого студента
+    for (st in stud.students){
+        for (gr in st.grades) {
+            if(gr.subject=="history") gr.mark=4 //Изменяем все оценки по истории на 4
         }
     }
-    writeFile("${path}new_$file_name.json",encodeJson(stud))
+    writeFile("${path}new_$file_name.json",encodeJson(stud))//сохраняем в json файл
 
-    stud.students[1].name="this is Cbor"
-    writeBinaryFile("${path}new_$file_name.cbor",encodeCbor(stud))
+    stud.students[1].name="this is Cbor"//Изменяем имя второго студента
+    writeBinaryFile("${path}new_$file_name.cbor",encodeCbor(stud))//сохраняем в бинарный файл в формате CBOR
     val stud_cbor=decodeCbor(readBinaryFile("${path}new_$file_name.cbor"))
-    println(stud_cbor)
+    println("read from cbor file: $stud_cbor")
 
-    stud.students[1].name="this is ProtoBuf"
-    writeBinaryFile("${path}new_$file_name.proto", encodeProtobuf(stud))
+    stud.students[1].name="this is ProtoBuf"//Изменяем имя второго студента
+    writeBinaryFile("${path}new_$file_name.proto", encodeProtobuf(stud))//сохраняем в бинарный файл в формате Protocol Buffer
     val stud_proto=decodeProtobuf(readBinaryFile("${path}new_$file_name.proto"))
-    print(stud_proto)
+    print("read from proto file: $stud_proto")
 }
-
 fun readFile(file_name:String)=File(file_name).readText()
 fun writeFile(file_name:String,content:String)=File(file_name).writeText(content)
 fun readBinaryFile(file_name: String)=File(file_name).readBytes()
--- src/main/kotlin/Students.kt
+++ src/main/kotlin/Students.kt
@@ -10,9 +10,12 @@ data class Student(var name:String, var group:String, var grades:Array<Grade>)
 @Serializable
 data class Grade(var subject:String, var mark:Int)
 
+//JSON
 fun decodeJson(strudents: String):Students=Json.decodeFromString<Students>(strudents)
 fun encodeJson(students: Students):String=Json.encodeToString(students)
+//CBOR
 fun decodeCbor(students: ByteArray):Students=Cbor.decodeFromByteArray<Students>(students)
 fun encodeCbor(students: Students):ByteArray=Cbor.encodeToByteArray(students)
+//ProtoBuff
 fun decodeProtobuf(students: ByteArray):Students=ProtoBuf.decodeFromByteArray<Students>(students)
 fun encodeProtobuf(students: Students):ByteArray=ProtoBuf.encodeToByteArray(students)
